0662a8e1d8cdd5b1df44fd11832fad0342a260ff
michaeldeluna <michael.deluna@gmail.com>
2016-02-28 22:49:37 +0800

    make it Windows friendly

--- src/main/java/com/danielflower/apprunner/runners/CommandLineProvider.java
+++ src/main/java/com/danielflower/apprunner/runners/CommandLineProvider.java
@@ -1,11 +1,12 @@
 package com.danielflower.apprunner.runners;
 
 import org.apache.commons.exec.CommandLine;
+import org.apache.commons.lang3.SystemUtils;
 
 import java.util.Map;
 
 public interface CommandLineProvider {
-    CommandLineProvider lein_on_path = (Map<String, String> m) -> new CommandLine("lein");
+    CommandLineProvider lein_on_path = (Map<String, String> m) -> new CommandLine(SystemUtils.IS_OS_WINDOWS ? "lein.bat" : "lein");
 
     CommandLine commandLine(Map<String, String> envVarsForApp);
 }
