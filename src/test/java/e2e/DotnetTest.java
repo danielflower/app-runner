3bc1a48f3012fddefab8ec8bdeddada434c15eb9
Evan Huang <caca0513@outlook.com>
2018-08-19 20:55:56 +0800

    1) add DotnetTest in e2e

---
+++ src/test/java/e2e/DotnetTest.java
@@ -0,0 +1,51 @@
+package e2e;
+
+import com.danielflower.apprunner.App;
+import com.danielflower.apprunner.Config;
+import com.danielflower.apprunner.runners.DotnetRunnerTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import scaffolding.AppRepo;
+import scaffolding.RestClient;
+
+import java.io.File;
+import java.util.HashMap;
+
+import static com.danielflower.apprunner.FileSandbox.fullPath;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static scaffolding.ContentResponseMatcher.equalTo;
+
+public class DotnetTest {
+
+    private final String port = "48186";
+    private final String appRunnerUrl = "http://localhost:" + port;
+    private final RestClient restClient = RestClient.create(appRunnerUrl);
+    private final String appId = "dotnet";
+    private final AppRepo appRepo = AppRepo.create(appId);
+
+    private final App app = new App(new Config(new HashMap<String, String>() {{
+        put(Config.SERVER_HTTP_PORT, port);
+        put(Config.DATA_DIR, fullPath(new File("target/datadirs/" + System.currentTimeMillis())));
+    }}));
+
+    @Before
+    public void start() throws Exception {
+        DotnetRunnerTest.ignoreTestIfNotSupported();
+        app.start();
+    }
+
+    @After
+    public void shutdownApp() {
+        app.shutdown();
+    }
+
+    @Test
+    public void canCreateAnAppViaTheRestAPI() throws Exception {
+        assertThat(restClient.createApp(appRepo.gitUrl()).getStatus(), is(201));
+        assertThat(restClient.deploy(appId).getStatus(), is(200));
+        assertThat(restClient.homepage(appId), is(equalTo(200, containsString(".NET Core sample project"))));
+    }
+}
